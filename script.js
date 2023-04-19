ef05c56251dbd1fe577d8f0f9890d348a7756f8a
Pulibanti Chaitanya <120620680+pulibanti@users.noreply.github.com>
2023-04-20 01:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function changeColor() {
+  var id = document.getElementById("input-id").value;
+  var color = document.getElementById("color").value;
+  var grid = document.getElementById(id);
+  var grids = document.querySelectorAll(".grid");
+  grids.forEach((element) => {
+    element.style.backgroundColor = "transparent";
+  });
+  grid.style.backgroundColor = color;
+}
